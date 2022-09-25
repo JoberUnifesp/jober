2522e2f5ddf14cac214fbe7a818a6152e72b2c6b
Junior Didzec <junior_didzec@hotmail.com>
2022-09-25 01:22:23 -0300

    login screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='wraper'>
+        <div className='logo'>
+          <h1 className='logo-text'>J</h1>
+          <h1 className='logo-text'>o</h1>
+          <h1 className='logo-text'>b</h1>
+          <h1 className='logo-text'>e</h1>
+          <h1 className='logo-text'>r</h1>
+        </div>
+        <div className='login-box'>
+          <h1 className='title'>Login</h1>
+          <input className='input-class' type='email' placeholder='Email'></input>
+          <input className='input-class' type='password' placeholder='Senha'></input>
+          <button className='enter-button'>Entrar</button>
+          <span className='bar-element'></span>
+          <button className='create-button'>Criar nova conta</button>
+        </div>
+      </div>
     </div>
   );
 }
