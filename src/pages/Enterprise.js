5c2c35242fda284c8816a10a9f38d22b84643f33
yasmin deodato <yasmin.deodato.beatriz@gmail.com>
2022-10-05 14:28:20 -0300

    add enterprise register integration to backend

--- src/pages/Enterprise.js
+++ src/pages/Enterprise.js
@@ -1,27 +1,59 @@
 import React from 'react';
 import './Enterprise.css';
 import Logo from '../components/Logo';
+import {useNavigate} from 'react-router-dom';
 
 function Enterprise() {
+  const navigate = useNavigate();
+    
   function handleSubmit(e) {
+    e.preventDefault();
+    
+    const newEnterprise = {
+      nome: e.target.enteprise_name.value,
+      email: e.target.email.value,
+      senha: e.target.senha.value,
+      cnpj: e.target.cnpj.value,
+      telefone: e.target.cellphone.value
+    }
+    
+    console.log(newEnterprise);
+
+    fetch('https://engsoft-jober.azurewebsites.net/company/SignUp', {
+      method: "POST",
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(newEnterprise)
+    })
+    .then((res) => res.json())
+    .then(data => { 
+      console.log(data)
+      if(data.code === 409){ 
+        //mostrar mensagem de erro na tela
+      }
+      if(data.code === 200){ 
+        navigate('/jober'); 
+      }
+    });
   }
 
   return (
-    <body className="App">
+    <div className="App">
       <div className='wraper'>
         <Logo/>
         <form className='enterprise-box' onSubmit={handleSubmit}>
           <div className='column-wrapper'>
             <div className='columns'>
-              <input className='input-enterprise' type='name' name='enteprise name' placeholder='Nome da empresa'></input>
+              <input className='input-enterprise' type='name' name='enteprise_name' placeholder='Nome da empresa'></input>
 
               <input className='input-enterprise' type='email' name='email' placeholder='Email'></input>
               <input className='input-enterprise' type='password' name='senha' placeholder='Senha'></input>
             </div>
             <div className='columns'>
               <input className='input-enterprise' type='tel' name='cnpj' placeholder='CNPJ'></input>
-              <input className='input-enterprise' type='tel' name='celphone' placeholder='Telefone'></input>
-              <input className='input-enterprise' type='password' name='senha' placeholder='Confirmar senha'></input>
+              <input className='input-enterprise' type='tel' name='cellphone' placeholder='Telefone'></input>
+              <input className='input-enterprise' type='password' placeholder='Confirmar senha'></input>
             </div>
           </div>
           <footer className='footer-term'>
@@ -34,7 +66,7 @@ function Enterprise() {
           <button className='enterprise-button' type='submit'>Cadastrar</button>
         </form>
       </div>
-    </body>
+    </div>
   );
 }
 
