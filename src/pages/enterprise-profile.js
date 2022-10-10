eb0ede07f64e875ba361f2b4bf5aedeef76c41ee
Junior Didzec <junior_didzec@hotmail.com>
2022-10-09 23:43:43 -0300

    Add css to enterpise profile page

--- src/pages/enterprise-profile.js
+++ src/pages/enterprise-profile.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import Logo from '../components/Logo';
+import './enterprise-profile.css';
+import mais from '../assets/mais.png';
 
 const id = localStorage.getItem('id')
 
@@ -69,84 +71,111 @@ function EnterpriseProfile() {
 //   }, []);
 
   return (
-    <body className="App">
-      <header>
-        <Logo/>
-      </header>
+    <>
+      <div className="eprofile">
+        <header className='header-eprofile'>
+            <div className='logo -eprofile'>
+              <h1 className='logo-text -eprofile'>J</h1>
+              <h1 className='logo-text -eprofile'>o</h1>
+              <h1 className='logo-text -eprofile'>b</h1>
+              <h1 className='logo-text -eprofile'>e</h1>
+              <h1 className='logo-text -eprofile'>r</h1>
+          </div>
+        </header>
 
-      <main className='wraper'>
-        <section>
-            <div className='circulo'></div>
-            <h2>Nome Sobrenome</h2>
-            <p contenteditable="true">Endereço</p>
-            <p contenteditable="true">E-mail</p>
-            <p contenteditable="true">Contato</p>
-            <h2>Quem somos?</h2>
-            <p contenteditable="true">Descrição Livre</p>
+        <main className='info-eprofile'>
+          <section className='secao-eprofile'>
+              <div className='img-eprofile'><p className='img-text-eprofile'>NS</p></div>
+              <h2 className='subtitle-eprofile'>Nome Sobrenome</h2>
+              <p contenteditable="true" className='input-info'>Endereço</p>
+              <p contenteditable="true" className='input-info'>E-mail</p>
+              <p contenteditable="true" className='input-info'>Contato</p>
+              <h2 className='subtitle-eprofile'>Quem somos?</h2>
+              <p contenteditable="true" className='input-info -who'>Descrição Livre</p>
+          </section>
 
-            <button className='enter-button' >Salvar</button>
-        </section>
-
-        <section>
+          <section className='wrapper-eprofile'>
             <div>
-                <h1>VagasAbertas</h1>
-                <button className='' onClick={() => {
-                    var temp = [...jobs]
-                    temp.splice(temp.length-1, temp.length);
-                    setJobs(temp);
-                }}>x</button>
-                <button className='' onClick={() => addJob()}>+</button>
-                <input className='input-class' value={newJob.Cargo} placeholder='Cargo' type='text' onChange={e => setNewJob({...newJob, Cargo: e.target.value})}></input>
-                <input className='input-class' value={newJob.Area} placeholder='Área' type='text' onChange={e => setNewJob({...newJob, Area: e.target.value})}></input>
-                <input className='input-class' value={newJob.SoftSkill1} placeholder='Soft Skill 01' type='text' onChange={e => setNewJob({...newJob, SoftSkill1: e.target.value})}></input>
-                <input className='input-class' value={newJob.SoftSkill2} placeholder='Soft Skill 02' type='text' onChange={e => setNewJob({...newJob, SoftSkill2: e.target.value})}></input>
-                <input className='input-class' value={newJob.SoftSkill3} placeholder='Soft Skill 03' type='text' onChange={e => setNewJob({...newJob, SoftSkill3: e.target.value})}></input>
-                <div>
-                    <input className='input-class' value={newJob.Experiencia} placeholder='Tempo Experiência' type='text' onChange={e => setNewJob({...newJob, Experiencia: e.target.value})}></input>
-                    <p>Meses</p>
+              <div>
+                <div className='div-title'>
+                  <h1  className='subtitle'>Vagas Abertas</h1>
+                  <div className='div-buttons'>
+                    <button className='add-button' onClick={() => addJob()}><img src={mais} alt="mais" className='image-mais'></img></button>
+                    <button className='exclude-button' onClick={() => {
+                        var temp = [...jobs]
+                        temp.splice(temp.length-1, temp.length);
+                        setJobs(temp);
+                    }}><img src={mais} alt="menos" className='image-menos'></img></button>
+                  </div>
                 </div>
-                <input className='input-class' value={newJob.Idioma} placeholder='Idioma' type='text' onChange={e => setNewJob({...newJob, Idioma: e.target.value})}></input>
-                <select value={newJob.NivelIdioma} onChange={e => setNewJob({...newJob, NivelIdioma: e.target.value})}>
-                  <option value=""></option>
-                  <option value="Basico">Básico</option>
-                  <option value="Intermediario">Intermediário</option>
-                  <option value="Avancado">avançado</option>
-                </select>
+                <div className='eprofile-box'>
+                  <div className='cargo-area'>
+                    <input className='input-eprofile' value={newJob.Cargo} placeholder='Cargo' type='text' onChange={e => setNewJob({...newJob, Cargo: e.target.value})}></input>
+                    <input className='input-eprofile' value={newJob.Area} placeholder='Área' type='text' onChange={e => setNewJob({...newJob, Area: e.target.value})}></input>
+                  </div>
+                  <div className='skills-area'>
+                    <input className='input-eprofile -skills' value={newJob.SoftSkill1} placeholder='Soft Skill 01' type='text' onChange={e => setNewJob({...newJob, SoftSkill1: e.target.value})}></input>
+                    <input className='input-eprofile -skills' value={newJob.SoftSkill2} placeholder='Soft Skill 02' type='text' onChange={e => setNewJob({...newJob, SoftSkill2: e.target.value})}></input>
+                    <input className='input-eprofile -skills' value={newJob.SoftSkill3} placeholder='Soft Skill 03' type='text' onChange={e => setNewJob({...newJob, SoftSkill3: e.target.value})}></input>
+                  </div>
+                  <div className='cidade-area'>
+                    <div className='div-temp'>
+                        <input className='input-eprofile -temp' value={newJob.Experiencia} placeholder='Tempo Experiência' type='text' onChange={e => setNewJob({...newJob, Experiencia: e.target.value})}></input>
+                        <p className='temp-text'>Meses</p>
+                    </div>
+                    <input className='input-eprofile -idioma' value={newJob.Idioma} placeholder='Idioma' type='text' onChange={e => setNewJob({...newJob, Idioma: e.target.value})}></input>
+                    <select className='select-eprofile' value={newJob.NivelIdioma} onChange={e => setNewJob({...newJob, NivelIdioma: e.target.value})}>
+                      <option value=""></option>
+                      <option value="Basico">Básico</option>
+                      <option value="Intermediario">Intermediário</option>
+                      <option value="Avancado">avançado</option>
+                    </select>
 
-                <input className='input-class' value={newJob.Cidade} placeholder='Cidade' type='text' onChange={e => setNewJob({...newJob, Cidade: e.target.value})}></input>
-                <input className='input-class' value={newJob.HardSkill1} placeholder='Hard Skill 01' type='text' onChange={e => setNewJob({...newJob, HardSkill1: e.target.value})}></input>
-                <select value={newJob.NivelHS1} onChange={e => setNewJob({...newJob, NivelHS1: e.target.value})}>
-                  <option value=""></option>
-                  <option value="Basico">Básico</option>
-                  <option value="Intermediario">Intermediário</option>
-                  <option value="Avancado">avançado</option>
-                </select>
+                    <input className='input-eprofile -idioma' value={newJob.Cidade} placeholder='Cidade' type='text' onChange={e => setNewJob({...newJob, Cidade: e.target.value})}></input>
+                  </div>
 
-                <input className='input-class' value={newJob.HardSkill2} placeholder='Hard Skill 01' type='text' onChange={e => setNewJob({...newJob, HardSkill2: e.target.value})}></input>
-                <select value={newJob.NivelHS2} onChange={e => setNewJob({...newJob, NivelHS2: e.target.value})}>
-                  <option value=""></option>
-                  <option value="Basico">Básico</option>
-                  <option value="Intermediario">Intermediário</option>
-                  <option value="Avancado">avançado</option>
-                </select>
-
-                <input className='input-class' value={newJob.HardSkill3} placeholder='Cargo' type='text' onChange={e => setNewJob({...newJob, HardSkill3: e.target.value})}></input>
-                <select value={newJob.NivelHS3} onChange={e => setNewJob({...newJob, NivelHS3: e.target.value})}>
-                  <option value=""></option>
-                  <option value="Basico">Básico</option>
-                  <option value="Intermediario">Intermediário</option>
-                  <option value="Avancado">avançado</option>
-                </select>
-
-                <ul>
-                    {jobs.map((item, index) => <li key={index}><p>{item.Cargo} {item.Area} {item.SoftSkill1} {item.Idioma}
-                                                {item.NivelIdioma} {item.Cidade} {item.HardSkill1} {item.NivelHS1} {item.HardSkill2}
-                                                {item.NivelHS2} {item.HardSkill3} {item.NivelHS3}</p></li>)}
-                </ul>
+                  <div className='skills-row'> 
+                    <div className='div-skills'>
+                      <input className='input-eprofile -hardskills' value={newJob.HardSkill1} placeholder='Hard Skill 01' type='text' onChange={e => setNewJob({...newJob, HardSkill1: e.target.value})}></input>
+                      <select className='select-eprofile -hardskills' value={newJob.NivelHS1} onChange={e => setNewJob({...newJob, NivelHS1: e.target.value})}>
+                        <option value=""></option>
+                        <option value="Basico">Básico</option>
+                        <option value="Intermediario">Intermediário</option>
+                        <option value="Avancado">avançado</option>
+                      </select>
+                    </div>
+                    <div className='div-skills'>
+                      <input className='input-eprofile -hardskills' value={newJob.HardSkill2} placeholder='Hard Skill 02' type='text' onChange={e => setNewJob({...newJob, HardSkill2: e.target.value})}></input>
+                      <select className='select-eprofile -hardskills' value={newJob.NivelHS2} onChange={e => setNewJob({...newJob, NivelHS2: e.target.value})}>
+                        <option value=""></option>
+                        <option value="Basico">Básico</option>
+                        <option value="Intermediario">Intermediário</option>
+                        <option value="Avancado">avançado</option>
+                      </select>
+                    </div>
+                    <div className='div-skills'>
+                      <input className='input-eprofile -hardskills' value={newJob.HardSkill3} placeholder='Hard Skill 03' type='text' onChange={e => setNewJob({...newJob, HardSkill3: e.target.value})}></input>
+                      <select className='select-eprofile -hardskills' value={newJob.NivelHS3} onChange={e => setNewJob({...newJob, NivelHS3: e.target.value})}>
+                        <option value=""></option>
+                        <option value="Basico">Básico</option>
+                        <option value="Intermediario">Intermediário</option>
+                        <option value="Avancado">avançado</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-        </section>
-      </main>
-    </body>
+            <ul>
+                {jobs.map((item, index) => <li className='eprofile-box' key={index}><p>{item.Cargo} {item.Area} {item.SoftSkill1} {item.Idioma}
+                                            {item.NivelIdioma} {item.Cidade} {item.HardSkill1} {item.NivelHS1} {item.HardSkill2}
+                                            {item.NivelHS2} {item.HardSkill3} {item.NivelHS3}</p></li>)}
+            </ul>
+            <button className='enter-button' >Salvar</button>
+          </section>
+        </main>
+      </div>
+    </>
   );
 }
 
