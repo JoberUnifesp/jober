60d29a737bea93f295c45e448367d94b0beaf85e
Beatriz Assumção da Silva <b.assumcao.da.silva@avanade.com>
2022-11-18 09:25:02 -0300

    Push candidates data from db

--- src/pages/candidates.js
+++ src/pages/candidates.js
@@ -3,49 +3,45 @@ import { useState, useEffect } from 'react';
 
 function Cadidates() {
     // const id = sessionStorage.getItem('meuid')
-    // const base_url = 'https://jober.azurewebsites.net'
+    const base_url = 'https://jober.azurewebsites.net'
   
     
     const [candidates, setCandidates] = useState([])
-    const [firstCandidate, setFirstCandidate] = useState({Id: "", Nome: "", Experiecias: [], Formacoes: [], HardSkilss: [], Idiomas: [], softSkils: [], like: ""})
-    const [secondCandidate, setSecondCandidate] = useState({Id: "", Nome: "", Experiecias: [], Formacoes: [], HardSkilss: [], Idiomas: [], softSkils: [], like: ""})
+    const [firstCandidate, setFirstCandidate] = useState({Id: "", Nome: "", Experiencias: [], Formacoes: [], HardSkills: [], Idiomas: [], softSkills: [], like: ""})
+    const [secondCandidate, setSecondCandidate] = useState({Id: "", Nome: "", Experiencias: [], Formacoes: [], HardSkills: [], Idiomas: [], softSkills: [], like: ""})
     const [saved, setSaved] = useState([])
     const [liked, setLiked] = useState([])
     const [count, setCount] = useState(0)
   
   
   
-    // useEffect(() => {  
-    //   fetch(`${base_url}/vacancy/allVacancies/${id}`, {
-    //     method: "GET",
-    //     headers: {
-    //       'Content-type': 'application/json'
-    //     }
-    //   })
-    //   .then(res => res.json())
-    //   .then((data)=> {
-    //     if (data !== null){
-    //       let temp = data.map((element) => {
-  
-    //         return {Id: element.ID, Nome: element.CARGO, Empresa: element.COMPANY_NAME, req1: `${element.HS_1} ${element.HS_1_NIVEL}`, req2: `${element.HS_2} ${element.HS_2_NIVEL}`, req3: `${element.HS_3} ${element.HS_3_NIVEL}`, like: element.like}
-    //       })
-  
-  
-    //       setFirstVacancie(temp[0])
-    //       temp.push({Nome: 'Loading...', Empresa: 'Loading...', req1: 'Loading...', req2: 'Loading...', req3: 'Loading...', like: false})
-    //       setVacancies(temp)
-  
-    //       console.log(temp)
-    //       console.log(vacancies)
+    useEffect(() => {  
+      fetch(`${base_url}/candidates/`, {
+        method: "GET",
+        headers: {
+          'Content-type': 'application/json'
+        }
+      })
+      .then(res => res.json())
+      .then((data)=> {
+        if (data !== null){
           
-    //     }
-    //   })
+          console.log(data[0])
+          let temp = data;
   
-    // }, []);
+  
+          setFirstCandidate(temp[0])
+          temp.push({Id: "Loading...", Nome: "Loading...", Experiencias: ["Loading..."], Formacoes: ["Loading..."], HardSkills: ["Loading..."], Idiomas: ["Loading..."], softSkills: ["Loading..."], like: "Loading..."})
+          setCandidates(temp)
+        
+        }
+      })
+  
+    }, []);
   
     function UpdateFunc() {
         var value = 'Teste ' + String(count)
-        setFirstCandidate({Id: value, Nome: value, Experiecias: [value, value, value], Formacoes: [value, value, value], HardSkilss: [value, value, value], Idiomas: [value, value, value], softSkils: [value, value, value], like: false})
+        setFirstCandidate({Id: value, Nome: value, Experiencias: [value, value, value], Formacoes: [value, value, value], HardSkills: [value, value, value], Idiomas: [value, value, value], softSkills: [value, value, value], like: false})
         setCandidates([...candidates, firstCandidate])
 
         setCount(v => v + 1)
@@ -163,15 +159,15 @@ function Cadidates() {
                 <p className='name-vacancies'>{firstCandidate.Nome}</p>
                 <hr className="hr1"></hr>
                 <p className='enterprise-vacancies'>Experiências: </p>
-                {firstCandidate.Experiecias.map(item => <p>{item}</p>)}
+                {firstCandidate.Experiencias.map(item => <p>{item}</p>)}
                 <p className='enterprise-vacancies'>Formações:</p>
                 {firstCandidate.Formacoes.map(item => <p>{item}</p>)}
                 <p className='enterprise-vacancies'>Hard Skils:</p>
-                {firstCandidate.HardSkilss.map(item => <p>{item}</p>)}
+                {firstCandidate.HardSkills.map(item => <p>{item}</p>)}
                 <p className='enterprise-vacancies'>Idiomas:</p>
                 {firstCandidate.Idiomas.map(item => <p>{item}</p>)}
                 <p className='enterprise-vacancies'>soft Skils:</p>
-                {firstCandidate.softSkils.map(item => <p>{item}</p>)}
+                {firstCandidate.softSkills.map(item => <p>{item}</p>)}
 
                 <p className={firstCandidate.like  ? 'like-text' : 'like-text -inv'}>🧡 A {firstCandidate.Empresa} gostou do seu perfil para esta vaga</p>
             </div>
@@ -180,15 +176,15 @@ function Cadidates() {
                 <p className='name-vacancies'>{secondCandidate.Nome}</p>
                 <hr className="hr1"></hr>
                 <p className='enterprise-vacancies'>Experiências:</p>
-                {secondCandidate.Experiecias.map(item => <p>{item}</p>)}
+                {secondCandidate.Experiencias.map(item => <p>{item}</p>)}
                 <p className='enterprise-vacancies'>Formações:</p>
                 {secondCandidate.Formacoes.map(item => <p>{item}</p>)}
                 <p className='enterprise-vacancies'>Hard Skils:</p>
-                {secondCandidate.HardSkilss.map(item => <p>{item}</p>)}
+                {secondCandidate.HardSkills.map(item => <p>{item}</p>)}
                 <p className='enterprise-vacancies'>Idiomas:</p>
                 {secondCandidate.Idiomas.map(item => <p>{item}</p>)}
                 <p className='enterprise-vacancies'>soft Skils:</p>
-                {secondCandidate.softSkils.map(item => <p>{item}</p>)}
+                {secondCandidate.softSkills.map(item => <p>{item}</p>)}
                 
                 <p className={secondCandidate.like  ? 'like-text' : 'like-text -inv'}>🧡 A {secondCandidate.Empresa} gostou do seu perfil para esta vaga</p>
             </div>
